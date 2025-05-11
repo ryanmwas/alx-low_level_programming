297901e73340b528b540e73f86b640cfc4c3177c
ryanmwas <mwangiryan78@gmail.com>
2025-05-11 07:46:48 +0300

    Print a statement

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+  * main - prints a statement using puts function
+  *
+  * Return: 0 if successful
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
