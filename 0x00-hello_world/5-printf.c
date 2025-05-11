e186551634900058a31519d16c0cc4f5b952f191
ryanmwas <mwangiryan78@gmail.com>
2025-05-11 07:52:56 +0300

    Print string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - starting point of the program
+*
+*Return: 0 if successful
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
