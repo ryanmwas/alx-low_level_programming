4a6af4a2804ec308bb44149407b854f79eb52470
ryanmwas <mwangiryan78@gmail.com>
2025-05-12 14:16:02 +0300

    Retrieve the last digit of n

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,11 +18,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 ld = n % 10;
-if (n > 5)
+if (ld > 5)
 {
 printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 }
-else if (n == 0)
+else if (ld == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, ld);
 }
