26341b643a48e6226e5c522243071a6c5a9a28e9
ryanmwas <mwangiryan78@gmail.com>
2025-05-12 14:43:05 +0300

    Print single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints numbers
+*
+* Return: 0
+*/
+int main(void)
+{
+int num = 0;
+while (num <= 9)
+{
+printf("%d", num);
+num++;
+}
+printf("\n");
+return (0);
+}
