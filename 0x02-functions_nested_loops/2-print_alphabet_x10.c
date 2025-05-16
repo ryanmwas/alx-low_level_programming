2174722d457d853e65914f988165aa65a2979cdf
ryanmwas <mwangiryan78@gmail.com>
2025-05-16 05:04:39 +0300

    Print lowercase alphabets 10 times

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,21 @@
 #include "main.h"
 /**
+* print_letters - print lowercase alphabets
+*
+* Return: none
+*/
+void print_letters(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+}
+void print_letters(void);
+/**
 * print_alphabet_x10 - prints lowercase alphabet 10 times
 *
 * Return: none
@@ -9,7 +25,7 @@ void print_alphabet_x10(void)
 int counter = 0;
 while (counter < 10)
 {
-print_alphabet();
+print_letters();
 counter++;
 }
 }
