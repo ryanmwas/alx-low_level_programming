5ee61e719d8d9db0f7a56570a371fc16ee3f2eb4
ryanmwas <mwangiryan78@gmail.com>
2025-05-29 20:34:15 +0300

    Pass the Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,20 +12,21 @@ for (num = 1; num <= 100; num++)
 {
 if (num % 15 == 0)
 {
-printf("FizzBuzz ");
+printf("FizzBuzz");
 }
 else if (num % 10 == 5 || num % 10 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else if (num % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else
 {
 printf("%d ", num);
 }
+printf(" ");
 }
 printf("\n");
 return (0);
