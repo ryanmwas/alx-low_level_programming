a9b20d13461a40125697d6c3f92b21b7a80fd7c6
ryanmwas <mwangiryan78@gmail.com>
2025-05-29 20:30:55 +0300

    Pass the Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - FizzBuzz
+*
+* Return: 0 if successfull
+*/
+int main(void)
+{
+int num;
+
+for (num = 1; num <= 100; num++)
+{
+if (num % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (num % 10 == 5 || num % 10 == 0)
+{
+printf("Buzz ");
+}
+else if (num % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", num);
+}
+}
+printf("\n");
+return (0);
+}
